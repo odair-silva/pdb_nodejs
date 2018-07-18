07e6de6c0f305450224c69b8c5c7ce0f946b935e
Bernardo Silva <bernardosilva@MacBook-Pro-de-Bernardo.local>
2018-07-18 04:51:32 -0300

    massa de dados

---
+++ dump.sql
@@ -0,0 +1,16 @@
+INSERT INTO especialidade (nome) VALUES('Traumatologia');
+INSERT INTO especialidade (nome)VALUES('Oftalmologia');
+INSERT INTO especialidade (nome) VALUES('Cardiologia');
+INSERT INTO especialidade (nome) VALUES('Dermatologia');
+INSERT INTO especialidade (nome) VALUES('Neurologia');
+INSERT INTO especialidade (nome) VALUES('Pediatria');
+
+INSERT INTO enfase (nome, especialidade_id) VALUES('Coluna',1);
+INSERT INTO enfase (nome, especialidade_id) VALUES('Ombro',1);
+INSERT INTO enfase (nome, especialidade_id) VALUES('Tórax',1);
+INSERT INTO enfase (nome, especialidade_id) VALUES('Cones',2);
+INSERT INTO enfase (nome, especialidade_id) VALUES('Bastonetes',2);
+INSERT INTO enfase (nome, especialidade_id) VALUES('Artérias',3);
+INSERT INTO enfase (nome, especialidade_id) VALUES('Cerebelo',5);
+INSERT INTO enfase (nome, especialidade_id) VALUES('Sinapses',5);
+
